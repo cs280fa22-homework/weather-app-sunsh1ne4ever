c92fb8ce925c375542dd7c5f3f3bd14392066b1c
Emma Levitsky <eecompcode@mail.com>
2022-09-22 13:26:32 -0400

    changed location for sound

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 
 const BASE_URL = "https://dataservice.accuweather.com";
-const API_KEY = "USE_YOUR_API_KEY"; // terrible practice!
+const API_KEY = "lYs2Gc2yBfd8wDYixLMIm0AAQxZgObNo"; // terrible practice!
 // You should never save API key directly in source code
 
 const search = document.getElementById("search");
@@ -14,19 +14,29 @@ function getWeatherForecast(event) {
 }
 
 function getLocationKey(city) {
-  // TODO get the "location key" for the given `city`!
-  //  then call getCurrentCondition to retrieve weather forecast for it!
+  fetch(`${BASE_URL}/locations/v1/cities/search?apikey=${API_KEY}&q=${city}`)
+  .then((response) => response.json())
+  .then((data) => {
+    const location = data[0];
+    getCurrentCondition(location);
+  })
+  .catch((err) => console.log(err));
   console.log(city);
 }
 
 function getCurrentCondition(location) {
-  // TODO get the "current condition" based on the `location` argument!
-  //  then call updateUI to update the UI!
+  const key = location.Key;
+  fetch(`${BASE_URL}/currentconditions/v1/${key}?apikey=${API_KEY}`)
+  .then((response) => response.json())
+  .then((data) => {
+    const condition = data[0];
+    updateUI(location, condition);
+  })
+  .catch((err) => console.log(err));
 }
 
 function updateUI(location, forecast) {
-  // TODO update the following based on `location` and `forecast` arguments!
-  document.getElementById("name").innerText = "City Name";
-  document.getElementById("condition").innerText = "Weather Condition";
-  document.getElementById("temperature").innerText = "Temperature";
+  document.getElementById("name").innerText = location.EnglishName;
+  document.getElementById("condition").innerText = forecast.WeatherText;
+  document.getElementById("temperature").innerText = forecast.Temperature.Metric.Value;
 }
